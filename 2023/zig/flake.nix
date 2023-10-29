7f57bef4c7335d1c2b752d1a9978d6d93974725f
aserowy <serowy@hotmail.com>
2023-10-29 14:31:35 +0100

    2023 zig 01: init
    Signed-off-by: aserowy <serowy@hotmail.com>

---
+++ 2023/zig/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "aoc zig 2023";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+
+  outputs = { self, nixpkgs, flake-parts }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            nil
+            nixpkgs-fmt
+            nodejs_20
+
+            zig
+          ];
+
+          shellHook = ''
+            '';
+        };
+      };
+    };
+}
